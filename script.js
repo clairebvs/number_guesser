76bcfd0cee3b61b41516e2408525971f20867cac
ClaireBvs <clairembeauvais@gmail.com>
2018-11-18 12:28:15 -0700

    add reset button and function [#1]

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-var randomNumber = Math.floor(Math.random() * 5 ) + 1;
+var randomNumber = Math.floor(Math.random() * 100) + 1;
 
 $('.user-submit').on('click', function () {
   var guessNumber = $('.user-number').val();
 
   if (parseInt(guessNumber) === randomNumber) {
+    $('.last-guess').text('');
     $('.message').text('BOOM!');
+    $('.hi-low').text('');
   } else if (parseInt(guessNumber) < randomNumber){
-    $('.message').text('That is too low');
+    $('.last-guess').text('Your last guess was');
+    $('.message').text(guessNumber);
+    $('.hi-low').text('That is too low');
   } else if (parseInt(guessNumber) > randomNumber){
-    $('.message').text('That is too high');
+    $('.last-guess').text('Your last guess was');
+    $('.message').text(guessNumber);
+    $('.hi-low').text('That is too high');
   }
 });
+
+resetButton.addEventListener('click', resetGame);
+
+function resetGame() {
+  var resetParas = document.querySelectorAll('.resultParas p');
+
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  userNumber.value = '';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
